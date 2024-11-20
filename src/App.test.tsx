1d64608f9f1948cebde4be5ec8cd9d2728ce3bee
Pratik Suryawanshi <83270697+pratiksury10@users.noreply.github.com>
2024-11-20 17:30:12 +0530

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,4 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-// src/App.test.tsx
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
